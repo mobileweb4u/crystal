8f13e7bebf1a2ba4c5fdaaf91360d291ddc64ea0
Freddie <freddierussell4u@gmail.com>
2022-06-30 20:45:10 +0100

    first commit

---
+++ src/pages/About.tsx
@@ -0,0 +1,52 @@
+export function About() {
+  return (
+    <>
+    About Me
+My primary skills started in server maintenance and coding. I expanded my skills and developed wide expertise in website building and mobile web applications.
+
+Aim to provide the right solution at the right time to our customers in order to allow the customer to achieve their aims and ambitions.
+
+Within the website design scope my strengths are;<p></p>
+
+
+👉️ Understanding business requirements.<p></p>
+
+👉️ Review of the initial design and checking for early functionality downfalls in design.<p></p>
+
+👉️ Fixing issues with existing websites.<p></p>
+
+👉️ Creating sustainable and innovative solutions.<p></p>
+
+👉️ We can Build fully custom JavaScript API for your business requirements.<p></p>
+
+👉️ Making recommendations where suitable to business need.<p></p>
+
+👉️ Web Design.<p></p>
+
+👉️ Brochure sites.<p></p>
+
+👉️ Membership sites.<p></p>
+
+👉️ Content managed sites.<p></p>
+
+👉️ Ecommerce sites.<p></p>
+
+👉️ Mobile friendly designs.<p></p>
+
+👉️ Internet Marketing.<p></p>
+
+👉️ Search engine optimization.<p></p>
+
+👉️ Social media marketing.<p></p>
+
+👉️ Email marketing.<p></p>
+
+👉️ Targeted leads for your business.<p></p>
+
+👉️ Fully Integrated.<p></p>
+
+👉️ With CRM and/or email marketing software creating a smooth Efficient experience.
+    </>
+  );
+  
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,19 @@
+export function Home() {
+  return (
+    <>
+    <h1>Welcome To my test site</h1>
+    
+  <p> Sign NDA so the project in this the collection presented includes functionalities of Next JS and wordpress predominantly. Examples that you see below are fictional sites created for the purpose of demonstrating my coding and design skills.</p>
+
+<p>These range from technical layer; such as website responsive/adaptive development using Elementor, both front-end and back-end, WordPress,Progressive Web App, design of both web and mobile projects for multi sized businesses</p>
+
+The following above link works <p></p>
+
+<p>Innovating one project at a time</p>
+      
+         
+   
+    </>
+  );
+  
+}
